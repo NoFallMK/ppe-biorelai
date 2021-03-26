62a1f0e64ba4fbaab5e20692c670403b11f9d266
Baptiste_Bnd <baptiste.bnd33@gmail.com>
2021-03-26 13:52:18 +0100

    Add Activities

---
+++ app/src/main/java/com/example/biorelai/ProducteurActivity.java
@@ -0,0 +1,14 @@
+package com.example.biorelai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProducteurActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_producteur);
+    }
+}
---
+++ app/src/main/java/com/example/biorelai/ResponsableActivity.java
@@ -0,0 +1,14 @@
+package com.example.biorelai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResponsableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_responsable);
+    }
+}
